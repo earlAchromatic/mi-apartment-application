28a5ec66a940d0bc956b5ca9b6a52db0ec8f8e44
earlAchromatic <68669571+earlAchromatic@users.noreply.github.com>
2022-03-26 01:12:39 -0400

    netlify forum fix using base64 encode

--- netlify/functions/send-pdf.js
+++ netlify/functions/send-pdf.js
@@ -40,47 +40,32 @@ exports.handler = async function (event) {
 };
 
 function parseMultipartForm(event) {
-  console.log('starting parse...');
   return new Promise((resolve) => {
-    // we'll store all form fields inside of this
     const fields = {};
 
-    // let's instantiate our busboy instance!
     const busboy = Busboy({
-      // it uses request headers
-      // to extract the form boundary value (the ----WebKitFormBoundary thing)
       headers: event.headers,
     });
 
-    // before parsing anything, we need to set up some handlers.
-    // whenever busboy comes across a file ...
-    busboy.on(
-      'file',
-      (fieldname, filestream, filename, transferEncoding, mimeType) => {
-        // ... we take a look at the file's data ...
-        filestream.on('data', (data) => {
-          // ... and write the file's name, type and content into `fields`.
-          fields[fieldname] = {
-            filename,
-            type: mimeType,
-            content: data,
-          };
-        });
-      }
-    );
+    busboy.on('file', (fieldname, filestream, filename, _, mimeType) => {
+      filestream.on('data', (data) => {
+        fields[fieldname] = {
+          content: data,
+          filename,
+          type: mimeType,
+        };
+      });
+    });
 
-    // whenever busboy comes across a normal field ...
     busboy.on('field', (fieldName, value) => {
-      // ... we write its value into `fields`.
       fields[fieldName] = value;
     });
 
-    // once busboy is finished, we resolve the promise with the resulted fields.
     busboy.on('finish', () => {
       resolve(fields);
     });
-    console.log('deep in the parse...');
-    // now that all handlers are set up, we can finally start processing our request!
-    busboy.write(event.body);
+
+    // This was the bastard!
+    busboy.write(Buffer.from(event.body, 'base64'));
   });
 }
